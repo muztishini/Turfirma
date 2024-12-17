2b8bd29eb7b13657a3831254f949baf7d2421b8f
muztishini <111636144+muztishini@users.noreply.github.com>
2024-12-17 09:57:34 +0500

    next commit

--- turfirmaproject/turfirmaapp/admin.py
+++ turfirmaproject/turfirmaapp/admin.py
@@ -7,6 +7,10 @@ admin.site.register(Tours)
 admin.site.register(Transport)
 admin.site.register(Excursions)
 admin.site.register(Hotels)
-# admin.site.register(HotelExcursion)
+admin.site.register(HotelExcursion)
 admin.site.register(Bookings)
 admin.site.register(Payments)
+
+admin.site.site_title = 'Админ-панель сайта турфирмы'
+admin.site.site_header = 'Админ-панель сайта турфирмы'
+admin.site.site_url = 'http://127.0.0.1:8000/turfirma/'
--- turfirmaproject/turfirmaapp/views.py
+++ turfirmaproject/turfirmaapp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h2>Hello, World!!!</h2>")
+    return render(request, 'index.html')
