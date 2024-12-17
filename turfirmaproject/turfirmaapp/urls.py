e76cb0a0aedc6fb4e574ca20d1468dc8b11226fb
muztishini <111636144+muztishini@users.noreply.github.com>
2024-12-17 12:24:42 +0500

    add about

--- turfirmaproject/turfirmaapp/urls.py
+++ turfirmaproject/turfirmaapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, about
 
 
 urlpatterns = [
     path('', index, name='home'),
-    # path('about', about, name='about'),
+    path('about', about, name='about'),
 ]
